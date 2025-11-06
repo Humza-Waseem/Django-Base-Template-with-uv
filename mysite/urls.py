3f7e60d079c0cff0af84d2dbb538ea26f6075782
Hamza Waseem <hamza.wasym99@gmail.com>
2025-11-06 15:33:09 +0500

    FIXED Users,Auth app

--- mysite/urls.py
+++ mysite/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('user/', include('users.urls')),
+    path('user/', include('apps.users.urls')),
+    path('auth/', include('apps.authentication.urls')),
 ]
